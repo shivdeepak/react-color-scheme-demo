c5b3b5afba417c506fb768a63ee2373cc6b56ee4
Shiv Deepak Muddada <shiv@aeroh.org>
2023-10-14 07:14:22 -0700

    [eslint] disable @typescript-eslint/no-explicit-any

--- .eslintrc.js
+++ .eslintrc.js
@@ -31,6 +31,7 @@ module.exports = {
         "react"
     ],
     "rules": {
-        "react/react-in-jsx-scope": "off"
+        "react/react-in-jsx-scope": "off",
+        "@typescript-eslint/no-explicit-any": "off",
     }
 }
